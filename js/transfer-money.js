9a2b5f8ce0dfcf672fb8f2eb110e5f89143373c7
Wojtek Kozioł <wojtek.koziol.pl@gmail.com>
2020-05-14 16:25:21 +0200

    Fix some bugs

--- js/transfer-money.js
+++ js/transfer-money.js
@@ -9,14 +9,11 @@ function transferMoney() {
     } else {
         for (let i = 0; i < data.length; i++) {
             if (data[i].firstName + " " + data[i].lastName === document.querySelector(".input-name").value) {
-                data[loggedIndex].balance -= document.querySelector(".amount-of-money").value;
-                data[i].balance += document.querySelector(".amount-of-money").value;
-
+                data[loggedIndex].balance -= parseInt(document.querySelector(".amount-of-money").value);
+                data[i].balance += parseInt(document.querySelector(".amount-of-money").value);
                 localStorage.setItem("data", JSON.stringify(data));
-
                 let h1 = document.createElement("h1");
                 h1.innerHTML = "Przelew wysłany";
-
                 document.querySelector(".page").innerHTML = "";
                 return document.querySelector(".page").appendChild(h1);
             }
